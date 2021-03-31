ec79dbdcc7ac6f1952ffe0341d4b9d934472218d
ZaibachX <35111459+ZaibachX@users.noreply.github.com>
2021-03-31 09:40:23 -0700

    Added a function and env file for sending out email notifications as well via gmail

--- vaccine.py
+++ vaccine.py
@@ -10,22 +10,37 @@ pip install beepy
 
 in your terminal.
 '''
-
-
-
 import requests
 import time
 import beepy
+import smtplib
+import ssl
+import yaml
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
 
+def load_env(): ###Update the .env.yml file with your information and save as env.yml (No period in front)
+    env = yaml.safe_load(open('env.yml'))
+
+    return env
+
+def send_email(user, pwd, email_address, email_to, body):
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login(user, pwd)
+    message = 'Subject: {}\n\n{}'.format('CVS Vaccine - Available', body)
+    s.sendmail(email_address,email_to,message)
 
 def findAVaccine():
     hours_to_run = 3 ###Update this to set the number of hours you want the script to run.
     max_time = time.time() + hours_to_run*60*60
     while time.time() < max_time:
 
-        state = 'IL' ###Update with your state abbreviation. Be sure to use all CAPS, e.g. RI
+        state = 'CA' ###Update with your state abbreviation. Be sure to use all CAPS, e.g. RI
 
-        response = requests.get("https://www.cvs.com/immunizations/covid-19-vaccine.vaccine-status.{}.json?vaccineinfo".format(state.lower()), headers={"Referer":"https://www.cvs.com/immunizations/covid-19-vaccine"})
+        response = requests.get("https://www.cvs.com/immunizations/covid-19-vaccine.vaccine-status.{}.json?vaccineinfo".format(state.lower()), 
+                                headers={"Referer":"https://www.cvs.com/immunizations/covid-19-vaccine"})
         payload = response.json()
 
         mappings = {}
@@ -33,18 +48,50 @@ def findAVaccine():
             mappings[item.get('city')] = item.get('status')
 
         print(time.ctime())
-        cities = ['BELLEVILLE', 'CHICAGO', 'DEKALB', 'WAUKEGAN'] ###Update with your cities nearby
+        ###Update with your cities nearby
+        cities = ['ALISO VIEJO',
+                  'CARLSBAD',
+                  'CHULA VISTA',
+                  'ENCINITAS',
+                  'ESCONDIDO',
+                  'LA JOLLA',
+                  'LA MESA',
+                  'MURRIETA',
+                  'NATIONAL CITY',
+                  'OCEANSIDE',
+                  'POWAY',
+                  'RAMONA',
+                  'SAN CLEMENTE',
+                  'SAN DIEGO',
+                  'SAN MARCOS',
+                  'SANTEE',
+                  'SOLANA BEACH',
+                  'SPRING VALLEY',
+                  'TEMECULA',
+                  'VISTA']
         for city in cities:
             print(city, mappings[city])
 
-        for key in mappings.keys():
-            if (key in cities) and (mappings[key] != 'Fully Booked'):
-                beepy.beep(sound = 'coin')
-                break
+        ### Created a dictionary to append multiple cities and send it out via gmail
+        cities_dict = dict()
+        for (key, value) in mappings.items():
+            if (value == 'Available') and (key in cities):
+                cities_dict[key] = value
             else:
                 pass
 
-        time.sleep(60) ##This runs every 60 seconds. Update here if you'd like it to go every 10min (600sec)
+        if len(cities_dict) > 0:
+            beepy.beep(sound = 'coin')
+            env = load_env()
+            send_email(env['user'],
+                            env['pwd'],
+                            env['user'],
+                            env['email_to'],
+                            env['body'].format(', '.join(cities_dict.keys()))
+                            )
+            break ###Feel free to remove the break to have the script continually send an email (when a city is available) if you'd like
+
+        time.sleep(60) ###This runs every 60 seconds. Update here if you'd like it to go every 10min (600sec)
         print('\n')
 
 findAVaccine() ###this final line runs the function. Your terminal will output the cities every 60seconds
